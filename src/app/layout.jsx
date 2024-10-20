4569fa5819f19b1fba78b96e27c51a64b818060c
pkurto16 <ptkurto3@gmail.com>
2024-10-20 18:28:21 -0400

    About page built

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,13 +1,15 @@
 import "./globals.css";
 import {NavigationMenu} from "@/components/ui/navigation-menu";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/footer";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
             <NavigationMenu className="width=500, height=100"></NavigationMenu>
-            <Navbar></Navbar>
-        {children}
+            <Navbar/>
+                {children}
+            <Footer/>
       </body>
     </html>
   );

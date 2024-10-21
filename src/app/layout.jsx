23f5e965c47de10a69d651b2939d5aba15206538
Mert Karabulut <mkarabul@purdue.edu>
2024-10-20 20:14:07 -0400

    Implement sticky navbar

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,12 +1,12 @@
 import "./globals.css";
-import {NavigationMenu} from "@/components/ui/navigation-menu";
+import { NavigationMenu } from "@/components/ui/navigation-menu";
 import Navbar from "@/components/Navbar";
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>
-            <NavigationMenu className="width=500, height=100"></NavigationMenu>
-            <Navbar></Navbar>
+    <html lang="en" className="h-full overflow-auto">
+      <body className="h-full overflow-auto">
+        {/* <NavigationMenu className="width=500, height=100"></NavigationMenu> */}
+        <Navbar></Navbar>
         {children}
       </body>
     </html>

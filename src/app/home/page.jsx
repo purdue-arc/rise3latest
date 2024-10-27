ab96f754cdf41f283ed458637aba8063ae5ad477
Mikah <mikah@mltd.com>
2024-10-27 17:34:05 -0400

    Linked registration button to registration link

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -33,13 +33,16 @@ export default function Page() {
           alt="RISE3 Conference"
           className="w-full h-auto object-cover"
         />
-        <div className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center text-center text-white bg-black bg-opacity-50">
+        <div
+            className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center text-center text-white bg-black bg-opacity-50">
           <h1 className="text-5xl font-bold mb-4">RISE3 Robotics Conference</h1>
           <p className="text-2xl mb-6">30th March 2025</p>
           <p className="text-xl mb-8">Largest Student Run Robotics Symposium</p>
           <a
-            href="#"
-            className="px-8 py-4 bg-blue-500 text-black rounded-md font-semibold hover:bg-blue-600"
+              href="https://riseforms.purduearc.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="px-8 py-4 bg-blue-500 text-black rounded-md font-semibold hover:bg-blue-600"
           >
             Register Your Interest
           </a>
